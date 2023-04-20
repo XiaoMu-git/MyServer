65b18a1c89cc6b11cd6ccd3dd4a04cbbe600d9c8
XiaoMu <13409006811@163.com>
2023-04-20 14:43:55 +0800

    优化 TinyTcpServer 服务器和客户端之间传输使用固定包头和结构体 使之更加结构化

--- TinyTcpServer/TinyTcpClient/TinyTcpClient.cpp
+++ TinyTcpServer/TinyTcpClient/TinyTcpClient.cpp
@@ -9,51 +9,91 @@
 
 using namespace std;
 
+enum CMD {
+	CMD_ERROR,
+	CMD_TRUE,
+	CMD_FALSE,
+	CMD_LOGIN,
+	CMD_LOGOUT
+};
+
+struct DataHeader {
+	short cmd_type;
+	short data_len;
+};
+
+struct LoginInfo {
+	char user_name[32];
+	char user_password[32];
+};
+
+struct LogoutInfo {
+	char user_name[32];
+};
+
+struct LoginResult {
+	int result;
+};
+
+struct LogoutResult {
+	int result;
+};
+
 int main() {
 	WORD var = MAKEWORD(2, 2);
-	WSAData dat;
+	WSADATA dat;
 	WSAStartup(var, &dat);
 
-	/*用Socket API建立简易TCP客户端*/
 	/*1.建立一个socket*/
-	SOCKET my_sock = socket(AF_INET, SOCK_STREAM, 0);
-	if (INVALID_SOCKET == my_sock) {
-		cout << "Socket创建失败。。。" << endl;
-	}
-	else {
-		cout << "Socket创建成功。。。" << endl;
-	}
+	SOCKET client_sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (client_sock == INVALID_SOCKET) cout << "Socket创建失败" << endl;
+	else cout << "Socket创建失败" << endl;
 
-	/*2.连接服务器 connect*/
-	sockaddr_in my_sockaddr = {};
-	my_sockaddr.sin_family = AF_INET;
-	my_sockaddr.sin_port = htons(4567);
-	my_sockaddr.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
-	if (connect(my_sock, (sockaddr*)&my_sockaddr, sizeof(sockaddr_in)) == SOCKET_ERROR) {
-		cout << "连接服务器失败。。。" << endl;
-	}
-	else {
-		cout << "连接服务器成功。。。" << endl;
-	}
+	/*2.连接服务器*/
+	sockaddr_in client_sockaddr_in;
+	client_sockaddr_in.sin_family = AF_INET;
+	client_sockaddr_in.sin_port = htons(4567);
+	client_sockaddr_in.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
+	if (connect(client_sock, (sockaddr*)&client_sockaddr_in, sizeof(sockaddr_in)) == SOCKET_ERROR) cout << "服务器连接失败" << endl;
+	else cout << "服务器连接成功" << endl;
 
 	while (true) {
-		char msg_buf[256] = {}, rec_buf[256] = {};
+
+		/*3.向服务器发送消息*/
+		char msg_buf[128];
 		cin >> msg_buf;
-		/*3.处理请求*/
-		if (!strcmp(msg_buf, "exit")) {
-			break;
+		DataHeader send_header;
+		DataHeader recv_header;
+		send_header.data_len = 0;
+		if (!strcmp(msg_buf, "exit")) break;
+		else if (!strcmp(msg_buf, "login")) {
+			LoginResult login_result;
+			LoginInfo login_info = { "xiaomu", "123456" };
+			send_header.cmd_type = CMD_LOGIN;
+			send(client_sock, (char*)&send_header, sizeof(DataHeader), 0);
+			send(client_sock, (char*)&login_info, sizeof(LoginInfo), 0);
+
+			/*4.接收服务器消息*/
+			recv(client_sock, (char*)&recv_header, sizeof(DataHeader), 0);
+			recv(client_sock, (char*)&login_result, sizeof(LoginResult), 0);
+
+			cout << "请求的结果是：" << login_result.result << endl;
 		}
+		else if (!strcmp(msg_buf, "logout")) {
+			LogoutResult logout_result;
+			LogoutInfo logout_info = { "xiaomu" };
+			send_header.cmd_type = CMD_LOGOUT;
+			send(client_sock, (char*)&send_header, sizeof(DataHeader), 0);
+			send(client_sock, (char*)&logout_info, sizeof(LogoutInfo), 0);
 
-		/*4.向服务器发送一条消息 send*/
-		send(my_sock, msg_buf, strlen(msg_buf) + 1, 0);
+			/*4.接收服务器消息*/
+			recv(client_sock, (char*)&recv_header, sizeof(DataHeader), 0);
+			recv(client_sock, (char*)&logout_result, sizeof(LoginResult), 0);
 
-		/*5.接收服务器信息 recv*/
-		recv(my_sock, rec_buf, 256, 0);
-		cout << rec_buf << endl;
+			cout << "请求的结果是：" << logout_result.result << endl;
+		}
+		else cout << "命令输入有误，请检查后重新输入" << endl;
 	}
-
-	/*6.关闭socket closesocket*/
-	closesocket(my_sock);
 
 	WSACleanup();
 	system("pause");
--- TinyTcpServer/TinyTcpServer/TinyTcpServer.cpp
+++ TinyTcpServer/TinyTcpServer/TinyTcpServer.cpp
@@ -9,92 +9,115 @@
 
 using namespace std;
 
+enum CMD {
+	CMD_ERROR,
+	CMD_TRUE,
+	CMD_FALSE,
+	CMD_LOGIN,
+	CMD_LOGOUT
+};
+
+struct DataHeader {
+	short cmd_type;
+	short data_len;
+};
+
+struct LoginInfo {
+	char user_name[32];
+	char user_password[32];
+};
+
+struct LogoutInfo {
+	char user_name[32];
+};
+
+struct LoginResult {
+	int result;
+};
+
+struct LogoutResult {
+	int result;
+};
+
 int main() {
 	WORD var = MAKEWORD(2, 2);
 	WSADATA dat;
 	WSAStartup(var, &dat);
 
-	/*用Socket API建立简易TCP服务端，并实现消息处理回传*/
 	/*1.建立一个socket*/
-	// 使用的协议簇：AF_INET，socket类型：SOCK_STREAM，协议的种类：IPPROTO_TCP
-	SOCKET my_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	if (INVALID_SOCKET == my_sock) {
-		cout << "socket创建失败。。。" << endl;
-	}
-	else {
-		cout << "socket创建成功。。。" << endl;
-	}
+	SOCKET server_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (INVALID_SOCKET == server_sock) cout << "socket创建失败" << endl;
+	else cout << "socket创建成功" << endl;
 
 	/*2.绑定接收客户端连接的端口 bind*/
-	// sockaddr_in比sockaddr更明确，所以更加好用
-	sockaddr_in my_sockaddr_in = {};
-	// 设置sock_in的类型，要与my_sock一致
-	my_sockaddr_in.sin_family = AF_INET;
-	// 设置sock_in的端口号，利用htons函数将主机中的数据类型转换为网络中的数据类型
-	my_sockaddr_in.sin_port = htons(4567);
-	// 绑定主机中的某个ip地址，INADDR_ANY意为主机所有ip地址
-	my_sockaddr_in.sin_addr.S_un.S_addr = INADDR_ANY;
-	if (bind(my_sock, (sockaddr*)&my_sockaddr_in, sizeof(sockaddr_in)) == SOCKET_ERROR) {
-		cout << "端口绑定失败。。。" << endl;
-	}
-	else {
-		cout << "端口绑定成功。。。" << endl;
-	}
+	sockaddr_in server_sock_in;
+	server_sock_in.sin_family = AF_INET;
+	server_sock_in.sin_port = htons(4567);
+	server_sock_in.sin_addr.S_un.S_addr = INADDR_ANY;
+	if (bind(server_sock, (sockaddr*)&server_sock_in, sizeof(sockaddr_in)) == SOCKET_ERROR) cout << "端口绑定失败" << endl;
+	else cout << "端口绑定成功" << endl;
 
 	/*3.监听网络端口 listen*/
-	// 正在等待连接的队列长度：5
-	if (listen(my_sock, 5) == SOCKET_ERROR) {
-		cout << "端口监听失败。。。" << endl;
-	}
-	else {
-		cout << "绑定监听成功。。。" << endl;
-	}
+	if (listen(server_sock, 5) == SOCKET_ERROR) cout << "端口监听失败" << endl;
+	else cout << "端口监听成功" << endl;
 
 	/*4.等待接收客户端连接 accept*/
-	sockaddr_in client_sockaddr = {};
-	int client_sockaddr_len = sizeof(client_sockaddr);
-	// 阻塞等待
-	SOCKET client_sock = accept(my_sock, (sockaddr*)&client_sockaddr, &client_sockaddr_len);
-	// INVALID_SOCKET意为无效的SOCKET
-	if (client_sock == INVALID_SOCKET) {
-		cout << "客户端连接失败。。。" << endl;
-	}
-	else {
-		// inet_ntoa可以把网络中的数据类型ip转换为主机中的数据类型
-		cout << "socket：" << (int)client_sock << "连接成功。。。" << endl;
-	}
+	sockaddr_in client_sockaddr_in;
+	int client_sockaddr_in_len = sizeof(sockaddr_in);
+	SOCKET client_sock = accept(server_sock, (sockaddr*)&client_sockaddr_in, &client_sockaddr_in_len);
+	if (client_sock == INVALID_SOCKET) cout << "客户端连接失败" << endl;
+	else cout << "客户端连接成功" << endl;
 
-	char rec_buf[256] = {};
 	while (true) {
-		/*5.接受客户端数据*/
-		int rec_buf_len = recv(client_sock, rec_buf, 256, 0);
-		if (rec_buf_len <= 0) {
-			cout << "客户端已退出。。。" << endl;
+		/*5.接收客户端消息 accept*/
+		DataHeader recv_header;
+		int recv_len = recv(client_sock, (char*)&recv_header, sizeof(DataHeader), 0);
+		if (recv_len <= 0) {
+			cout << "客户端以断开链接" << endl;
 			break;
 		}
 
-		cout << "收到命令：" << rec_buf << endl;
-		/*6.处理请求*/
-		if (!strcmp(rec_buf, "getName")) {
-			char msg_buf[256] = "张三";
-			/*7.向客户端发送一条数据 send*/
-			send(client_sock, msg_buf, strlen(msg_buf) + 1, 0);
-		}
-		else if (!strcmp(rec_buf, "getAge")) {
-			char msg_buf[256] = "20";
-			/*7.向客户端发送一条数据 send*/
-			send(client_sock, msg_buf, strlen(msg_buf) + 1, 0);
-		}
-		else {
-			char msg_buf[256] = "经检查输入是否有误。。。";
-			/*7.向客户端发送一条数据 send*/
-			send(client_sock, msg_buf, strlen(msg_buf) + 1, 0);
+		cout << "收到命令类型：" << recv_header.cmd_type << "，数据长度：" << recv_len << endl;
+
+		/*回复客户端消息*/
+		DataHeader send_header;
+		switch (recv_header.cmd_type) {
+		case CMD_LOGIN:
+			LoginInfo login_info;
+			LoginResult login_result;
+			send_header.cmd_type = CMD_LOGIN;
+			send_header.data_len = 0;
+			recv(client_sock, (char*)&login_info, sizeof(LoginInfo), 0);
+			cout << "用户名：" << login_info.user_name << "，密码：" << login_info.user_password << endl;
+			if (login_info.user_name == "xiaomu" && login_info.user_password == "123456") login_result.result = CMD_TRUE;
+			else login_result.result = CMD_FALSE;
+			send(client_sock, (char*)&send_header, sizeof(DataHeader), 0);
+			send(client_sock, (char*)&login_result, sizeof(LoginResult), 0);
+			break;
+
+		case CMD_LOGOUT:
+			LogoutInfo logout_info;
+			LogoutResult logout_result;
+			send_header.cmd_type = CMD_LOGOUT;
+			send_header.data_len = 0;
+			recv(client_sock, (char*)&logout_info, sizeof(LogoutInfo), 0);
+			if (logout_info.user_name == "xiaomu") logout_result.result = CMD_TRUE;
+			else logout_result.result = CMD_FALSE;
+			send(client_sock, (char*)&send_header, sizeof(DataHeader), 0);
+			send(client_sock, (char*)&logout_result, sizeof(LogoutResult), 0);
+			break;
+
+		default:
+			send_header.cmd_type = CMD_ERROR;
+			send_header.data_len = 0;
+			send(client_sock, (char*)&send_header, sizeof(DataHeader), 0);
+			break;
 		}
 	}
 
-	/*8.关闭socket closesocket*/
+	/*关闭socket*/
 	closesocket(client_sock);
-	closesocket(my_sock);
+	closesocket(server_sock);
 
 	WSACleanup();
 	system("pause");
