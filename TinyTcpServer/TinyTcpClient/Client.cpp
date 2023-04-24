59f7fa9b3a50c9ea4c60080af89c7a780c493fbb
XiaoMu <13409006811@163.com>
2023-04-24 08:35:27 +0800

    基于Tcp Socket Select thread 简易聊天群项目 已完成

--- TinyTcpServer/TinyTcpClient/Client.cpp
+++ TinyTcpServer/TinyTcpClient/Client.cpp
@@ -5,17 +5,19 @@
 #include <WinSock2.h>
 #include <vector>
 #include <algorithm>
+#include <thread>
 
 #pragma comment(lib, "ws2_32.lib")
 
 enum CMD {
-	CMD_FALSE,
+	CMD_ERROR,
 	CMD_TREU,
+	CMD_FALSE,
 	CMD_LOGIN,
-	CMD_INQUIRE,
+	CMD_LOGOUT,
 	CMD_RESPONSE,
-	CMD_USERJOIN,
-	CMD_ERROR
+	CMD_MESSAGE,
+	CMD_USERINFO
 };
 
 class Header {
@@ -25,25 +27,6 @@ public:
 	Header() {
 		this->length = sizeof(Header);
 		this->cmd_type = -1;
-	}
-};
-
-class Login : public Header {
-public:
-	char username[32];
-	char password[32];
-	Login() {
-		this->length = sizeof(Login);
-		this->cmd_type = CMD_LOGIN;
-	}
-};
-
-class Inquire : public Header {
-public:
-	char inquire[64];
-	Inquire() {
-		this->length = sizeof(Inquire);
-		this->cmd_type = CMD_INQUIRE;
 	}
 };
 
@@ -57,36 +40,114 @@ public:
 	}
 };
 
-class UserJoin : public Header {
+class Message : public Header {
 public:
-	int sock;
-	UserJoin() {
-		this->length = sizeof(UserJoin);
-		this->cmd_type = CMD_USERJOIN;
+	char msg[128];
+	char username[32];
+	SOCKET send_socket;
+	Message() {
+		this->length = sizeof(Message);
+		this->cmd_type = CMD_MESSAGE;
 	}
 };
 
+class UserInfo : public Header {
+public:
+	char username[32];
+	char password[32];
+	SOCKET socket;
+	UserInfo() {
+		this->length = sizeof(UserInfo);
+	}
+};
+
+UserInfo my_info;
+SYSTEMTIME sysTime = { 0 };
+
+void doLogin(SOCKET client_socket) {
+	while (true) {
+		GetSystemTime(&sysTime);
+		Sleep(1000);
+		system("cls");
+		UserInfo send_user_info;
+		Response recv_response;
+		char cmd[256];
+		printf("[%02d:%02d:%02d]", sysTime.wHour + 8, sysTime.wMinute, sysTime.wSecond);
+		printf("请先登录账户\n");
+		printf("username: "); scanf("%s", cmd);
+		strcpy(send_user_info.username, cmd);
+		printf("password: "); scanf("%s", cmd);
+		strcpy(send_user_info.password, cmd);
+		send_user_info.cmd_type = CMD_LOGIN;
+		send_user_info.socket = client_socket;
+		send(client_socket, (char*)&send_user_info, send_user_info.length, 0);
+		recv(client_socket, (char*)&recv_response, recv_response.length, 0);
+		printf("[%02d:%02d:%02d]", sysTime.wHour + 8, sysTime.wMinute, sysTime.wSecond);
+		if (recv_response.resp == CMD_TREU) {
+			strcpy(my_info.username, send_user_info.username);
+			strcpy(my_info.password, send_user_info.password);
+			printf("登陆成功!\n");
+			return;
+		}
+		else printf("账号或密码错误，请重试。。。\n");
+	}
+}
+
+void cmdThread(SOCKET client_socket) {
+	while (true) {
+		GetSystemTime(&sysTime);
+		Message recv_message;
+		recv_message.send_socket = client_socket;
+		strcpy(recv_message.username, my_info.username);
+		scanf("%s", recv_message.msg);
+		if (recv_message.msg[0] == '/') {
+			// 命令区块
+		}
+		else send(client_socket, (char*)&recv_message, recv_message.length, 0);
+	}
+}
+
 bool processMessages(SOCKET client_socket) {
+	GetSystemTime(&sysTime);
 	char recv_temp[4096];
 	if (recv(client_socket, recv_temp, sizeof(Header), 0) <= 0) {
+		printf("[%02d:%02d:%02d]", sysTime.wHour + 8, sysTime.wMinute, sysTime.wSecond);
 		printf("与服务器断开连接\n", client_socket);
 		return false;
 	}
 	Header* recv_header = (Header*)recv_temp;
 
 	switch (recv_header->cmd_type) {
-	case CMD_USERJOIN: {
-		recv(client_socket, recv_temp + sizeof(Header), recv_header->length - sizeof(Header), 0);
-		UserJoin* recv_user_join = (UserJoin*)recv_temp;
-		printf("新客户端加入<Socket=%d>\n", recv_user_join->sock);
-	}
-	default: {
-		break;
-	}
+		case CMD_MESSAGE: {
+			recv(client_socket, recv_temp + sizeof(Header), recv_header->length - sizeof(Header), 0);
+			Message* recv_message = (Message*)recv_temp;
+			if (recv_message->send_socket != client_socket) {
+				printf("[%02d:%02d:%02d]", sysTime.wHour + 8, sysTime.wMinute, sysTime.wSecond);
+				printf("%s: %s\n", recv_message->username, recv_message->msg);
+			}
+			break;
+		}
+		case CMD_LOGIN: {
+			recv(client_socket, recv_temp + sizeof(Header), recv_header->length - sizeof(Header), 0);
+			UserInfo* recv_user_info = (UserInfo*)recv_temp;
+			if (recv_user_info->socket != client_socket) {
+				printf("[%02d:%02d:%02d]", sysTime.wHour + 8, sysTime.wMinute, sysTime.wSecond);
+				printf("[系统消息]用户%s加入群聊\n", recv_user_info->username);
+			}
+			break;
+		}
+		case CMD_LOGOUT: {
+			recv(client_socket, recv_temp + sizeof(Header), recv_header->length - sizeof(Header), 0);
+			UserInfo* recv_user_info = (UserInfo*)recv_temp;
+			printf("[%02d:%02d:%02d]", sysTime.wHour + 8, sysTime.wMinute, sysTime.wSecond);
+			printf("[系统消息]用户%s退出群聊\n", recv_user_info->username);
+			break;
+		}
 	}
 }
 
 int main() {
+	GetSystemTime(&sysTime);
 	/*启动 Windows socket 2.2环境*/
 	WORD ver = MAKEWORD(2, 2);
 	WSADATA dat;
@@ -94,20 +155,35 @@ int main() {
 
 	/*建立客户端 socket*/
 	SOCKET client_socket = socket(AF_INET, SOCK_STREAM, 0);
-	if (client_socket == INVALID_SOCKET) printf("错误，建立socket失败\n");
-	else printf("建立socket成功\n");
+	printf("[%02d:%02d:%02d]", sysTime.wHour + 8, sysTime.wMinute, sysTime.wSecond);
+	if (client_socket == INVALID_SOCKET) {
+		printf("错误，建立socket失败。。。\n");
+		system("pause");
+		return 0;
+	}
+	else printf("建立socket成功。。。\n");
 
 	/*连接服务器*/
 	sockaddr_in client_sockaddr_in;
 	client_sockaddr_in.sin_family = AF_INET;
 	client_sockaddr_in.sin_port = htons(4567);
-	client_sockaddr_in.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
+	client_sockaddr_in.sin_addr.S_un.S_addr = inet_addr("59.110.170.223");
+	printf("[%02d:%02d:%02d]", sysTime.wHour + 8, sysTime.wMinute, sysTime.wSecond);
 	if (connect(client_socket, (sockaddr*)&client_sockaddr_in, sizeof(client_sockaddr_in)) == SOCKET_ERROR) {
-		printf("错误，服务器连接失败\n");
+		printf("错误，服务器连接失败。。。\n");
+		system("pause");
+		return 0;
 	}
-	else printf("服务器连接成功\n");
+	else printf("服务器连接成功。。。\n");
+
+	// 账户登录
+	doLogin(client_socket);
+
+	/*开启多线程用来输入 thread*/
+	std::thread putIn(cmdThread, client_socket);
 
 	while (true) {
+		GetSystemTime(&sysTime);
 		fd_set fd_read;
 		FD_ZERO(&fd_read);
 		FD_SET(client_socket, &fd_read);
@@ -115,6 +191,7 @@ int main() {
 		/*开启 select 服务*/
 		timeval time_val = { 1, 0 };
 		if (select(client_socket + 1, &fd_read, NULL, NULL, &time_val) < 0) {
+			printf("[%02d:%02d:%02d]", sysTime.wHour + 8, sysTime.wMinute, sysTime.wSecond);
 			printf("select服务结束\n");
 			break;
 		}
@@ -122,15 +199,11 @@ int main() {
 		if (FD_ISSET(client_socket, &fd_read)) {
 			FD_CLR(client_socket, &fd_read);
 			if (!processMessages(client_socket)) {
+				printf("[%02d:%02d:%02d]", sysTime.wHour + 8, sysTime.wMinute, sysTime.wSecond);
 				printf("select服务结束\n");
 				break;
 			}
 		}
-
-		Inquire send_inquire;
-		strcpy(send_inquire.inquire, "This is a test.");
-		send(client_socket, (char*)&send_inquire, send_inquire.length, 0);
-		Sleep(500);
 	}
 
 	/*关闭 socket*/
--- TinyTcpServer/TinyTcpServer/Server.cpp
+++ TinyTcpServer/TinyTcpServer/Server.cpp
@@ -5,20 +5,18 @@
 #include <WinSock2.h>
 #include <vector>
 #include <algorithm>
-#include <time.h>
 
 #pragma comment(lib, "ws2_32.lib")
 
-std::vector<SOCKET> vec_client_sockets;
-
 enum CMD {
-	CMD_FALSE,
+	CMD_ERROR,
 	CMD_TREU,
+	CMD_FALSE,
 	CMD_LOGIN,
-	CMD_INQUIRE,
+	CMD_LOGOUT,
 	CMD_RESPONSE,
-	CMD_USERJOIN,
-	CMD_ERROR
+	CMD_MESSAGE,
+	CMD_USERINFO
 };
 
 class Header {
@@ -28,25 +26,6 @@ public:
 	Header() {
 		this->length = sizeof(Header);
 		this->cmd_type = -1;
-	}
-};
-
-class Login : public Header {
-public:
-	char username[32];
-	char password[32];
-	Login() {
-		this->length = sizeof(Login);
-		this->cmd_type = CMD_LOGIN;
-	}
-};
-
-class Inquire : public Header {
-public:
-	char inquire[64];
-	Inquire() {
-		this->length = sizeof(Inquire);
-		this->cmd_type = CMD_INQUIRE;
 	}
 };
 
@@ -60,56 +39,94 @@ public:
 	}
 };
 
-class UserJoin : public Header {
+class Message : public Header {
 public:
-	int sock;
-	UserJoin() {
-		this->length = sizeof(UserJoin);
-		this->cmd_type = CMD_USERJOIN;
+	char msg[128];
+	char username[32];
+	SOCKET send_socket;
+	Message() {
+		this->length = sizeof(Message);
+		this->cmd_type = CMD_MESSAGE;
 	}
 };
 
-bool processMessages(SOCKET client_socket) {
-	SYSTEMTIME sysTime = { 0 };
-	GetSystemTime(&sysTime);
+class UserInfo : public Header {
+public:
+	char username[32];
+	char password[32];
+	SOCKET socket;
+	UserInfo() {
+		this->length = sizeof(UserInfo);
+	}
+};
 
+std::vector<UserInfo> clients_info;
+SYSTEMTIME sysTime = { 0 };
+
+bool processMessages(SOCKET client_socket) {
+	GetSystemTime(&sysTime);
 	char recv_temp[4096];
 	if (recv(client_socket, recv_temp, sizeof(Header), 0) <= 0) {
-		printf("[%d:%d:%d]客户端<Socket=%d>已退出，连接断开\n", sysTime.wHour + 8, sysTime.wMinute, sysTime.wSecond, client_socket);
+		for (auto client : clients_info) {
+			if (client.socket == client_socket) {
+				UserInfo send_user_info;
+				strcpy(send_user_info.username, client.username);
+				send_user_info.socket = client.socket;
+				send_user_info.cmd_type = CMD_LOGOUT;
+				for (auto client : clients_info) {
+					send(client.socket, (char*)&send_user_info, send_user_info.length, 0);
+				}
+			}
+		}
+		printf("[%02d:%02d:%02d]", sysTime.wHour + 8, sysTime.wMinute, sysTime.wSecond);
+		printf("客户端<Socket=%d>已退出，连接断开\n", client_socket);
 		return false;
 	}
-	Header *recv_header = (Header*)recv_temp;
+	Header* recv_header = (Header*)recv_temp;
 
 	switch (recv_header->cmd_type) {
-	case CMD_LOGIN: {
-		recv(client_socket, recv_temp + sizeof(Header), recv_header->length - sizeof(Header), 0);
-		Login* recv_login = (Login*)&recv_temp;
-		Response send_response;
-		printf("[%d:%d:%d]收到客户端<Socket=%d>请求：CMD_LOGIN，数据长度：%d，userName=%s PassWord=%s\n", sysTime.wHour + 8, sysTime.wMinute, sysTime.wSecond, client_socket, recv_login->length, recv_login->username, recv_login->password);
-		if (recv_login->username == "xiaomu" && recv_login->password == "123456") {
-			send(client_socket, (char*)&send_response, send_response.length, 0);
+		case CMD_LOGIN: {
+			// 处理登录请求
+			recv(client_socket, recv_temp + sizeof(Header), recv_header->length - sizeof(Header), 0);
+			UserInfo* recv_user_info = (UserInfo*)recv_temp;
+			Response send_response;
+			printf("[%02d:%02d:%02d]", sysTime.wHour + 8, sysTime.wMinute, sysTime.wSecond);
+			printf("收到客户端<Socket=%d>请求：CMD_LOGIN，数据长度：%d，userName=%s\n", client_socket, recv_user_info->length, recv_user_info->username);
+			if (!strcmp(recv_user_info->password, "123456")) {
+				send(client_socket, (char*)&send_response, send_response.length, 0);
+				for (auto client : clients_info) {
+					send(client.socket, (char*)recv_user_info, recv_user_info->length, 0);
+				}
+			}
+			else {
+				send_response.resp = CMD_FALSE;
+				send(client_socket, (char*)&send_response, send_response.length, 0);
+			}
+
+			for (auto iter = clients_info.begin(); iter != clients_info.end(); iter++) {
+				if ((*iter).socket == client_socket) {
+					strcpy((*iter).username, recv_user_info->username);
+					strcpy((*iter).password, recv_user_info->password);
+					break;
+				}
+			}
+			break;
 		}
-		else {
-			send_response.resp = CMD_FALSE;
-			send(client_socket, (char*)&send_response, send_response.length, 0);
+		case CMD_MESSAGE: {
+			// 转发客户端消息
+			recv(client_socket, recv_temp + sizeof(Header), recv_header->length - sizeof(Header), 0);
+			Message* recv_message = (Message*)recv_temp;
+			printf("[%02d:%02d:%02d]", sysTime.wHour + 8, sysTime.wMinute, sysTime.wSecond);
+			printf("收到客户端<Socket=%d>请求：CMD_MESSAGE，数据长度：%d\n", client_socket, recv_message->length);
+			for (auto client : clients_info) {
+				send(client.socket, (char*)recv_message, recv_message->length, 0);
+			}
 		}
-		break;
-	}
-	case CMD_INQUIRE: {
-		recv(client_socket, recv_temp + sizeof(Header), recv_header->length - sizeof(Header), 0);
-		Inquire *recv_inquire = (Inquire*)recv_temp;
-		printf("[%d:%d:%d]收到客户端<Socket=%d>请求：CMD_INQUIRE，数据长度：%d，\n", sysTime.wHour + 8, sysTime.wMinute, sysTime.wSecond, client_socket, recv_inquire->length);
-	}
-	default: {
-		Response send_response;
-		send_response.resp = CMD_ERROR;
-		send(client_socket, (char*)&send_response, send_response.length, 0);
-		break; 
-	}
 	}
 }
 
 int main() {
+	GetSystemTime(&sysTime);
 	/*启动 Windows socket 2.2环境*/
 	WORD ver = MAKEWORD(2, 2);
 	WSADATA dat;
@@ -117,7 +134,8 @@ int main() {
 
 	/*建立服务器 socket*/
 	SOCKET server_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	if (server_socket == INVALID_SOCKET) printf("[%d:%d:%d]错误，建立socket失败\n");
+	printf("[%02d:%02d:%02d]", sysTime.wHour + 8, sysTime.wMinute, sysTime.wSecond);
+	if (server_socket == INVALID_SOCKET) printf("错误，建立socket失败\n");
 	else printf("建立socket成功\n");
 
 	/*绑定服务器端口*/
@@ -125,66 +143,70 @@ int main() {
 	sock_addr_in.sin_family = AF_INET;
 	sock_addr_in.sin_port = htons(4567);
 	sock_addr_in.sin_addr.S_un.S_addr = INADDR_ANY;
+	printf("[%02d:%02d:%02d]", sysTime.wHour + 8, sysTime.wMinute, sysTime.wSecond);
 	if (bind(server_socket, (sockaddr*)&sock_addr_in, sizeof(sock_addr_in)) == SOCKET_ERROR) {
 		printf("错误，绑定端口失败\n");
 	}
 	else printf("绑定端口成功\n");
 
 	/*监听服务器端口*/
+	printf("[%02d:%02d:%02d]", sysTime.wHour + 8, sysTime.wMinute, sysTime.wSecond);
 	if (listen(server_socket, 5) == SOCKET_ERROR) {
 		printf("错误，端口监听失败\n");
 	}
 	else printf("端口监听成功\n");
 
 	while (true) {
-		SYSTEMTIME sysTime = { 0 };
 		GetSystemTime(&sysTime);
-
 		fd_set fd_read;
 		FD_ZERO(&fd_read);
 		FD_SET(server_socket, &fd_read);
 
-		for (auto socket : vec_client_sockets) FD_SET(socket, &fd_read);
+		// 将新连接的 socket 加入 fd_read
+		for (auto client : clients_info) FD_SET(client.socket, &fd_read);
 
 		/*开启 select 服务*/
 		timeval time_val = { 1, 0 };
 		if (select(server_socket + 1, &fd_read, NULL, NULL, &time_val) < 0) {
+			printf("[%02d:%02d:%02d]", sysTime.wHour + 8, sysTime.wMinute, sysTime.wSecond);
 			printf("select服务结束\n");
 			break;
 		}
 
+		// 判断服务器 socket 存活
 		if (FD_ISSET(server_socket, &fd_read)) {
 			FD_CLR(server_socket, &fd_read);
 			/*等待客户端连接*/
 			sockaddr_in client_sockaddr_in;
 			int client_sockaddr_in_len = sizeof(sockaddr_in);
 			SOCKET client_socket = accept(server_socket, (sockaddr*)&client_sockaddr_in, &client_sockaddr_in_len);
+			printf("[%02d:%02d:%02d]", sysTime.wHour + 8, sysTime.wMinute, sysTime.wSecond);
 			if (client_socket == INVALID_SOCKET) {
 				printf("错误，收到无效客户端sock\n");
 			}
 			else {
-				printf("[%d:%d:%d]新客户端加入<Socket=%d><IP=%s>\n", sysTime.wHour + 8, sysTime.wMinute, sysTime.wSecond, client_socket, inet_ntoa(client_sockaddr_in.sin_addr));
-				for (auto socket : vec_client_sockets) {
-					UserJoin send_user_join;
-					send_user_join.sock = client_socket;
-					send(socket, (char*)&send_user_join, send_user_join.length, 0);
-				}
-				vec_client_sockets.push_back(client_socket);
+				printf("新客户端加入<Socket=%d><IP=%s>\n", client_socket, inet_ntoa(client_sockaddr_in.sin_addr));
+				UserInfo new_client;
+				new_client.socket = client_socket;
+				clients_info.push_back(new_client);
 			}
 		}
 
+		// 移除容器中断连的客户端
 		for (int i = 0; i < fd_read.fd_count; i++) {
 			if (!processMessages(fd_read.fd_array[i])) {
-				auto iter = find(vec_client_sockets.begin(), vec_client_sockets.end(), fd_read.fd_array[i]);
-				if (iter != vec_client_sockets.end()) {
-					vec_client_sockets.erase(iter);
+				for (auto iter = clients_info.begin(); iter != clients_info.end(); iter++) {
+					if ((*iter).socket == fd_read.fd_array[i]) {
+						clients_info.erase(iter);
+						break;
+					}
 				}
 			}
 		}
 	}
 
 	/*关闭所有 socket*/
-	for (auto socket : vec_client_sockets) closesocket(socket);
+	for (auto client : clients_info) closesocket(client.socket);
 	closesocket(server_socket);
 
 	/*关闭 Windows socket 2.2环境*/
