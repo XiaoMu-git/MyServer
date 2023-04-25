767b36a36ba6d57b9ab8beff0591d68cce4e9606
XiaoMu <13409006811@163.com>
2023-04-25 09:36:54 +0800

    C++面向对象封装客户端 未完成

--- TinyTcpServer/TinyTcpClient/Client.cpp
+++ TinyTcpServer/TinyTcpClient/Client.cpp
@@ -27,15 +27,80 @@ bool Client::initSocket() {
 // 连接服务器
 bool Client::doConnect(const char* ip = "127.0.0.1", int post = 2976) {
 	if (_socket == INVALID_SOCKET) initSocket();
-	_sockaddr.sin_family = AF_INET;
-	_sockaddr.sin_port = htons(post);
+	_addr.sin_family = AF_INET;
+	_addr.sin_port = htons(post);
 #ifdef _WIN32
-	_sockaddr.sin_addr.S_un.S_addr = inet_addr(ip);
+	_addr.sin_addr.S_un.S_addr = inet_addr(ip);
 #else
-	_sockaddr.sin_addr.s_addr = inet_addr(ip);
+	_addr.sin_addr.s_addr = inet_addr(ip);
 #endif
-	int res = connect(_socket, (sockaddr*)&_sockaddr, sizeof(_sockaddr));
+	int res = connect(_socket, (sockaddr*)&_addr, sizeof(_addr));
 	return res != SOCKET_ERROR;
+}
+
+// 接收消息
+bool Client::doRecv() {
+	int res = recv(_socket, _recv, sizeof(Header), 0);
+	if (res <= 0) return false;
+	Header* h_temp = (Header*)_recv;
+	int h_size = sizeof(Header);
+	recv(_socket, _recv + h_size, h_temp->length - h_size, 0);
+}
+
+// 发送消息
+bool Client::doSend() {
+	int res = send(_socket, _recv, sizeof(Header), 0);
+	switch (((Header*)_recv)->cmd_type) {
+	case CMD_MESSAGE: {
+		// 接收message消息
+		recv(_socket, recv_temp + sizeof(Header), recv_header->length - sizeof(Header), 0);
+		Message* recv_message = (Message*)recv_temp;
+		break;
+	}
+	case CMD_LOGIN: {
+		// 接收他人login消息
+		recv(_socket, recv_temp + sizeof(Header), recv_header->length - sizeof(Header), 0);
+		UserInfo* recv_user_info = (UserInfo*)recv_temp;
+		break;
+	}
+	case CMD_LOGOUT: {
+		// 接收他人logout消息
+		recv(_socket, recv_temp + sizeof(Header), recv_header->length - sizeof(Header), 0);
+		UserInfo* recv_user_info = (UserInfo*)recv_temp;
+		break;
+	}
+	}
+}
+
+// 登陆服务器
+bool Client::doLogin() {
+	Response recv_response;
+	_info.cmd_type = CMD_LOGIN;
+	_info.socket = _socket;
+	send(_socket, (char*)&_info, _info.length, 0);
+	recv(_socket, (char*)&recv_response, recv_response.length, 0);
+	if (recv_response.resp == CMD_TREU) return true;
+	else return false;
+}
+
+// 开启服务
+bool Client::doRun(const timeval time_val = { 1, 0 }) {
+	fd_set fd_read; FD_ZERO(&fd_read); FD_SET(_socket, &fd_read);
+	int res = select(_socket + 1, &fd_read, NULL, NULL, &time_val);
+	if (res < 0) return false;
+
+	if (FD_ISSET(_socket, &fd_read)) {
+		FD_CLR(_socket, &fd_read);
+		return dispose();
+	}
+
+	return true;
+}
+
+// 处理消息
+bool Client::dispose() {
+
+
 }
 
 // 关闭socket
--- TinyTcpServer/TinyTcpClient/Client.h
+++ TinyTcpServer/TinyTcpClient/Client.h
@@ -6,7 +6,9 @@
 class Client {
 private:
 	SOCKET _socket;
-	sockaddr_in _sockaddr;
+	sockaddr_in _addr;
+	UserInfo _info;
+	char _recv[4096];
 
 public:
 	Client();
@@ -19,10 +21,23 @@ public:
 	// 连接服务器
 	bool doConnect(const char* ip, int post);
 
+	// 接收消息
+	bool doRecv();
+
+	// 发送消息
+	bool doSend();
+
+	// 登录服务器
+	bool doLogin();
+
+	// 开启服务
+	bool doRun(timeval time_val);
+
+	// 处理消息
+	bool dispose();
+
 	// 关闭socket
 	bool doClose();
-
-	// 
 };
 
 #endif
